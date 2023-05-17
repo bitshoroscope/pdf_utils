7702d3efcef7f86a8121584a2d2100f1ad9e2038
andy bravo <andres.bravo@propiedades.com>
2023-05-17 15:12:44 -0400

    added fuctionality for splitting and extracting

--- split.py
+++ split.py
@@ -1,15 +1,55 @@
+import argparse
+import sys
 from PyPDF2 import PdfReader, PdfWriter
 
-file_name = 'cert.pdf'
-pages = (1, 120)
-
-reader = PdfReader(file_name)
 writer = PdfWriter()
-page_range = range(pages[0], pages[1] + 1)
 
-for page_num, page in enumerate(reader.pages, 1):
-    if page_num in page_range:
-        writer.add_page(page)
+def extract(file_name, pages):
+    """Extracts the specified pages from the PDF file."""
+    reader = PdfReader(file_name)
+    writer = PdfWriter()
 
-with open(f'output.pdf', 'wb') as out:
-    writer.write(out)
+    start, ending = map(int, pages.split(","))
+    page_range = range(start, ending + 1)
+
+    for page_num, page in enumerate(reader.pages, 1):
+        if page_num in page_range:
+            writer.add_page(page)
+
+    with open(f'output.pdf', 'wb') as out:
+        writer.write(out)
+
+
+def split(file_name, n):
+    """Splits the PDF file into n equal parts."""
+    reader = PdfReader(file_name)
+    num_pages = len(reader.pages)
+
+    page_size = num_pages // n
+
+    for i in range(n):
+        writer = PdfWriter()
+        for j in range(page_size):
+            writer.add_page(reader.pages[j + i * page_size])
+
+        with open(f'output{i + 1}.pdf', 'wb') as out:
+            writer.write(out)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file_name", help="The name of the PDF file")
+    parser.add_argument("action", help="The action to perform")
+    parser.add_argument("args", help="The arguments for the action")
+
+    args = parser.parse_args()
+
+    if args.action == "extract":
+        extract(args.file_name, args.args)
+    elif args.action == "split":
+        split(args.file_name, int(args.args))
+    else:
+        print("Invalid action.")
+
+if __name__ == "__main__":
+    main()
